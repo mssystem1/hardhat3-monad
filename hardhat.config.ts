a696717e0b7e3c951d7a1c2c637e14c630554b04
portdeveloper <108868128+portdeveloper@users.noreply.github.com>
2025-11-17 10:31:03 -0300

    Add monvision verif details

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,50 +1,36 @@
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
-import { configVariable, defineConfig } from "hardhat/config";
+import { defineConfig } from "hardhat/config";
 import "dotenv/config";
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 export default defineConfig({
   plugins: [hardhatToolboxViemPlugin],
   solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
-        version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
     },
   },
   networks: {
-    hardhatMainnet: {
+    hardhat: {
       type: "edr-simulated",
-      chainType: "l1",
-    },
-    hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
-    },
-    monadMainnet: {
-      type: "http",
-      chainType: "l1",
-      url: configVariable("MONAD_MAINNET_RPC_URL"),
-      accounts: [configVariable("MONAD_MAINNET_PRIVATE_KEY")],
     },
     monadTestnet: {
       type: "http",
-      chainType: "l1",
-      url: configVariable("MONAD_TESTNET_RPC_URL"),
-      accounts: [configVariable("MONAD_TESTNET_PRIVATE_KEY")],
-    },
+      url: "https://testnet-rpc.monad.xyz",
+      accounts: [PRIVATE_KEY],
+      chainId: 10143,
+    }
   },
   verify: {
-    etherscan: {
-      apiKey: configVariable("ETHERSCAN_API_KEY"),
-    },
-  },
+    sourcify: {
+      enabled: true,
+      apiUrl: "https://sourcify-api-monad.blockvision.org",
+    }
+  }
 });
